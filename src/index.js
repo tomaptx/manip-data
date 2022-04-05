6043b5b89e16a53bad14b31a40bfcbfd518ad183
Tom <tomaptx@users.noreply.github.com>
2022-04-05 12:58:52 -0600

    working version with button id

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 import { companiesData } from "./companies-data";
 import { salesData } from "./sales-data";
 
-const before = document.getElementById("before");
-before.innerHTML = JSON.stringify(companiesData, null, 2);
+//const before = document.getElementById("before");
+//before.innerHTML = JSON.stringify(companiesData, null, 2);
 
 //functions are here
-const manipulatedData = companiesData[0].fieldData;
-const keys = Object.keys(manipulatedData);
+//const manipulatedData = companiesData[0].fieldData;
+//const manipulatedData = companiesData.map(function (e) { 
+//const manipulatedData = companiesData.filter (function (e) { 
+//    const firstLetter = e.fieldData.CompanyName.charAt(0); 
+//    return firstLetter === "M";
+//    return firstLetter === "M";
+//    
+//}).map(function (e) { 
+//    return {
+//        state: e.fieldData.State,
+//        name: e.fieldData.CompanyName  
+//    } 
+//});
 
+function clickMe(e) {
+    alert(e.target.id);
+};
 const after = document.getElementById("after");
-after.innerHTML = JSON.stringify (keys, null, 2);
+
+const createButton = function (e) {
+    const button = document.createElement("button");
+    button.innerHTML = e.fieldData.CompanyName;
+    button.addEventListener("click", clickMe);
+    button.type = "button";
+    button.id = e.fieldData.Id;
+    button.className = "col-2 btn btn-outline-primary m-1"
+
+    after.appendChild(button);
+}
+companiesData.forEach( createButton )
+//const keys = Object.keys(manipulatedData);
+
+const isArray = Array.isArray(companiesData);
+//console.log isArray;
+
+after.innerHTML = JSON.stringify (manipulatedData, null, 2);
 
